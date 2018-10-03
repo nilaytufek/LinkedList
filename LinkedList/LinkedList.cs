1a0107142948c5d9e7a1f16acbaee195382b7e9c
nilaytufek <ntufek@gmail.com>
2018-10-03 02:12:24 +0100

    Add files via upload

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class Node<V>
+    {
+        public V value;
+        public Node(V v)
+        {
+            value = v;
+        }
+        public Node<V> NextNode = null;
+    }
+    class LinkedList<V>
+    {
+        public Node<V> first = null;
+        public Node<V> last = null;
+        
+        public LinkedList()
+        {
+            first = null;
+            last = first;
+        }
+        public void Add(Node<V> node)
+        {
+            if (first == null)
+            {
+                first = node;
+            }
+            else
+            {
+                last.NextNode = node;
+            }
+            last = node;
+        }
+        public void Remove(Node<V> node)
+        {
+            Node<V> candidate = first;
+            if (node == candidate)
+            {
+                first = first.NextNode;
+                return;
+            }
+            while (Comparer<Node<V>>.Default.Compare(candidate.NextNode, node) != 0)
+            {
+                candidate = candidate.NextNode;
+            }
+            candidate.NextNode = candidate.NextNode.NextNode;
+
+        }
+        public V[] GetList()
+        {
+            V[] arr = new V[0];
+            Node<V> tmp = first;
+            int i = 0;
+            while (tmp != null)
+            {
+                Array.Resize(ref arr, i + 1);
+                arr[i] = tmp.value;
+                tmp = tmp.NextNode;
+                i++;
+                
+            }
+            return arr;
+        } 
+        public bool Search(V val)
+        {
+            Node<V> tmp = first;
+            while(tmp != null)
+            {
+                if(Comparer<V>.Default.Compare(tmp.value, val) == 0)
+                {
+                    return true;
+                }
+                tmp = tmp.NextNode;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<int> myList = new LinkedList<int>();
+            Node<int> n1 = new Node<int>(4);
+            Node<int> n2 = new Node<int>(2);
+            Node<int> n3 = new Node<int>(0);
+            Console.WriteLine("adding the node with value: " + n1.value);
+            myList.Add(n1);
+            Console.WriteLine("my linked list: " + string.Join(" ", myList.GetList()));
+            Console.WriteLine("adding the node with value: " + n2.value);
+            myList.Add(n2);
+            Console.WriteLine("my linked list: " + string.Join(" ", myList.GetList()));
+            Console.WriteLine("is " + n2.value + " in the list? : " + myList.Search(n2.value));
+            Console.WriteLine("adding the node with value: " + n3.value);
+            myList.Add(n3);
+            Console.WriteLine("my linked list: " + string.Join(" ", myList.GetList()));
+
+            Console.WriteLine("removing the node with value: " + n2.value);
+            myList.Remove(n2);
+            Console.WriteLine("my linked list: " + string.Join(" ", myList.GetList()));
+            Console.WriteLine("is " + n2.value + " in the list? : " + myList.Search(n2.value));
+            Node<int> n4 = new Node<int>(6);
+            Console.WriteLine("adding the node with value: " + n4.value);
+            myList.Add(n4);
+            Console.WriteLine("my linked list: " + string.Join(" ", myList.GetList()));
+
+            
+            
+        }
+    }
+}
